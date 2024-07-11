313bc24ba876dc85eef4346a8c100d83934b23aa
Viktor Pirogov <Blade-85@inbox.ru>
2024-07-11 16:18:49 +0300

    поиск по строке

--- src/app/components/listUsers/listUsers.tsx
+++ src/app/components/listUsers/listUsers.tsx
@@ -3,12 +3,11 @@ import { getUsersBySearch } from '@/app/api/userApi';
 import { useEffect, useState } from 'react';
 // import ExampleUseEffect from '../useEffect/useEffect';
 
-
 interface Items {
   items: any[];
 }
 export default function Home() {
-  const [search, setSearch] = useState('555');
+  const [search, setSearch] = useState('');
   const [data, setData] = useState<Items | null>(null);
   useEffect(() => {
     getUsersBySearch({ q: search }).then((res) => {
@@ -18,9 +17,16 @@ export default function Home() {
   console.log(data);
   return (
     <div>
-      {/* <ExampleUseEffect /> */}
-      <input  placeholder="Введите пользователя" />
-      <ul>{data && data.items.map((el) => <li key={el}>{el.login}</li>)}</ul>
+      <input
+        type="text"
+        onChange={(event) => setSearch(event.target.value)}
+        placeholder="Введите пользователя"
+      />
+      <ul>
+        {data?.items
+          ? data.items.map((el) => <li key={el}>{el.login}</li>)
+          : search === ''}
+      </ul>
     </div>
   );
 }
